c8d4542d8a96937ae35ea5cda7c805f570b01f67
luisreyes26 <43220993+luisreyes26@users.noreply.github.com>
2018-12-09 07:21:30 -0600

    Clase Division de Matrices-Segunda Rvision
    clase division d ematrices

--- src/matrix/MatrizDiv.java
+++ src/matrix/MatrizDiv.java
@@ -1,5 +1,6 @@
 
-import javax.swing.JOptionPane;
+
+import java.util.Scanner;
 
 /*
  * To change this license header, choose License Headers in Project Properties.
@@ -13,25 +14,57 @@ import javax.swing.JOptionPane;
  */
 public class MatrizDiv {
     
-    
-    //int matriza[][]=new int[4][4];
-
-    private int filas;
-    private int columnas;
-    private int matrizA[][]=new int[filas][columnas];
-    private int matrizB[][]=new int[filas][columnas];
-    private int matrizRes[][]=new int[filas][columnas];
+     Scanner dato = new Scanner (System.in);
+    private int MatrizA[][];
+    private int MatrizB[][];
+    private int MatrizResul[][];
     
     
-    public void Div(){
-        System.out.println("Division de las dos matrices:");
-        for(int i=0;i<filas;i++){
-            for(int j=0;j<columnas;j++){
-                matrizRes[i][j]=matrizA[i][j]/matrizB[i][j];                            
-                System.out.print(matrizRes[i][j]+" ");
-                }
-            System.out.println();
+    
+    //CONSTRUCTOR
+    public void MatrizDiv(int MatrizA[][], int MatrizB[][]){
+        this.MatrizA=MatrizA;
+        this.MatrizB=MatrizB;
+    }
+    
+    
+    public void LlenarMatrices(){
+        int filaA =MatrizA.length;
+        int columnaA = MatrizA[0].length;
+        int filaB =MatrizB.length;
+        int columnaB = MatrizB[0].length;
+        System.out.println("Datos de la MATRIZ A: ");
+        for (int i=0; i<filaA; i++) {
+            for(int j=0; j<columnaA; j++){
+                System.out.println("Escribir valor " + i + " , " + j + " : ");
+                MatrizA [i][j]=dato.nextInt();
+            }
+        }
+        
+        System.out.println("DATOS DE LA MATRIZ B: " );
+        for(int i=0; i<filaB; i++){
+            for(int j=0; j<columnaB; j++){
+                System.out.println("Escribir valor " + i + " , " + j + " : ");
+                MatrizB [i][j]=dato.nextInt();
+            }
         }
     }
     
+    public void DivisionMatrices(){
+        for (int i=0; i<=2; i++){
+             for (int j=0; j<=2; j++){
+                 MatrizResul [i][j]= MatrizA[i][j]/MatrizB[i][j];
+                }
+            }
+    }
+    
+    public void imprimir(){
+        for (int i=0;i<=2;i++){
+            for (int j=0;j<=2;j++) {
+                System.out.print(MatrizResul[i][j] + " ");
+            }
+            System.out.println("");
+            
+        }
+    }   
 }
